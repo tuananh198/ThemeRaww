41171fc7a25e14d02ef627e898795be3f7b78f89
tuananh198 <ngoanhtuan1311998@gmail.com>
2021-01-19 13:56:31 +0700

    ThemeRaww

---
+++ AHT/raww/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    map: {
+        '*': {
+            'qty-counter': 'Magento_Catalog/js/qty-counter'
+        }
+    },
+    paths: {
+        slick: 'js/slick',
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        }
+    }
+};
